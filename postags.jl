02c17c0f2c68e280c50b8d8fc63f4d99066c704d
ozanarkancan <can.ozanarkan@gmail.com>
2017-02-21 20:04:29 +0300

    most occured tag

--- postags.jl
+++ postags.jl
@@ -14,7 +14,17 @@ grid, jelly, l = getallinstructions()
 for ins in vcat(grid, jelly, l)
     l = nltk.pos_tag(ins.text)
     for (w,t) in l
-        d[w] = t
+        if haskey(d, w)
+            if haskey(d[w], t)
+                d[w][t] = d[w][t] + 1
+            else
+                d[w][t] = 1
+            end
+        else
+            d[w] = Dict()
+            d[w][t] = 1
+        end
+
         if haskey(count, w)
             count[w] = count[w] + 1
         else
@@ -24,7 +34,9 @@ for ins in vcat(grid, jelly, l)
 end
 
 for w in keys(d)
-    push!(df, (w, d[w], count[w]))
+    println("Word: $w -> $(d[w])")
+    t = sort(collect(d[w]);rev=true, by=x->x[2])[1][1]
+    push!(df, (w, t, count[w]))
 end
 
 writetable("data/postags.csv", df)
