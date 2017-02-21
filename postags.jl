597da368084617ee3ac617514d627b8c1d528af2
ozanarkancan <can.ozanarkan@gmail.com>
2017-02-21 14:11:48 +0300

    counts

--- postags.jl
+++ postags.jl
@@ -5,8 +5,9 @@ nltk = PyCall.pywrap(PyCall.pyimport("nltk"))
 
 using DataFrames
 
-df = DataFrame(word=Any[], tag=Any[])
+df = DataFrame(word=Any[], tag=Any[], count=Any[])
 d = Dict()
+count = Dict()
 
 grid, jelly, l = getallinstructions()
 
@@ -14,11 +15,16 @@ for ins in vcat(grid, jelly, l)
     l = nltk.pos_tag(ins.text)
     for (w,t) in l
         d[w] = t
+        if haskey(count, w)
+            count[w] = count[w] + 1
+        else
+            count[w] = 1
+        end
     end
 end
 
 for w in keys(d)
-    push!(df, (w, d[w]))
+    push!(df, (w, d[w], count[w]))
 end
 
 writetable("data/postags.csv", df)
