90d17627b84f9b6d43d79f97f3ccbc32437f93e4
ozanarkancan <can.ozanarkan@gmail.com>
2017-02-27 15:39:03 +0300

    fix

--- wvec.jl
+++ wvec.jl
@@ -5,21 +5,23 @@ using PyCall
 global const word2vec = PyCall.pywrap(PyCall.pyimport("gensim.models.word2vec"))
 
 type Wvec
-	model
+    model
 end
 
 function Wvec(fname::String; bin=true)
-	Wvec(word2vec.Word2Vec["load_word2vec_format"](fname, binary=bin))
+    Wvec(word2vec.Word2Vec["load_word2vec_format"](fname, binary=bin))
 end
 
 function getvec(m::Wvec, word::AbstractString)
-	vec = nothing
-	try
-		vec = m.model["__getitem__"](word)
-	catch
-		vec = m.model["__getitem__"]("unk")
-	end
-	return vec
+    vec = nothing
+    unk = false
+    try
+        vec = m.model["__getitem__"](word)
+    catch
+        vec = m.model["__getitem__"]("unk")
+        unk = true
+    end
+    return vec, unk
 end
 
 export Wvec;
