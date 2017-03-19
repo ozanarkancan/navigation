5429f244b286a39f8c7356383f599fab675814b8
ocan13 <ocan13@biyofiz-4-1.local>
2017-03-19 13:22:03 +0300

    turn and move, move and turn with lang only second part

--- datagen/data_generator.jl
+++ datagen/data_generator.jl
@@ -183,7 +183,7 @@ function move_to_x(name, id)
     return ins, navimap
 end
 
-to_x(name, id) = rand([turn_to_x, move_to_x])(name, id)
+combined_12(name, id) = rand([turn_to_x, move_to_x])(name, id)
 
 function turn_and_move_to_x(name, id)
     h,w = (8,8)
@@ -279,6 +279,37 @@ function turn_and_move_to_x(name, id)
     return ins, navimap
 end
 
+function turncands(p)
+    steps = length(p)-1
+    a = action(p[1], p[2])
+    cands = Any[]
+    d = a == 2 ? "right" : "left"
+    if steps == 1
+        push!(cands, string("turn ", d))
+        push!(cands, string("turn ", d, " one time"))
+        push!(cands, string("turn ", d, " once"))
+    else
+        push!(cands, string("turn ", d, " two times"))
+        push!(cands, string("turn ", d, " twice"))
+    end
+    return cands
+end
+
+function movecands(p)
+    steps = length(p)-1
+    cands = Any[]
+    for prfx in ["move ", "go ", "walk "]
+        for d in ["","forward "]
+            for t in numbers[steps]
+                for b in (steps == 1 ? [" step", " segment", " block"] : [" steps", " segments", " blocks"])
+                    push!(cands, string(prfx, d, t, b))
+                end
+            end
+        end
+    end
+    return cands
+end
+
 function lang_only(name, id)
     h,w = (8,8)
     ins = nothing
@@ -303,37 +334,6 @@ function lang_only(name, id)
 
             mname = join(rand(CHARS, 20))
             navimap.name = mname
-
-            function turncands(p)
-                steps = length(p)-1
-                a = action(p[1], p[2])
-                cands = Any[]
-                d = a == 2 ? "right" : "left"
-                if steps == 1
-                    push!(cands, string("turn ", d))
-                    push!(cands, string("turn ", d, " one time"))
-                    push!(cands, string("turn ", d, " once"))
-                else
-                    push!(cands, string("turn ", d, " two times"))
-                    push!(cands, string("turn ", d, " twice"))
-                end
-                return cands
-            end
-
-            function movecands(p)
-                steps = length(p)-1
-                cands = Any[]
-                for prfx in ["move ", "go ", "walk "]
-                    for d in ["","forward "]
-                        for t in numbers[steps]
-                            for b in (steps == 1 ? [" step", " segment", " block"] : [" steps", " segments", " blocks"])
-                                push!(cands, string(prfx, d, t, b))
-                            end
-                        end
-                    end
-                end
-                return cands
-            end
 
             if r == 1
                 cands = turncands(path)
@@ -372,14 +372,198 @@ end
 
 combined_1245(name, id) = rand([turn_to_x, move_to_x, turn_and_move_to_x, lang_only])(name, id)
 
+function turn_to_x_and_move(name, id)
+    h,w = (8,8)
+    ins = nothing
+    navimap = nothing
+
+    while ins == nothing
+        maze, available = generate_maze(h, w; numdel=1)
+
+        r = rand(1:3)#1:item, #2:floor, #3:wall
+        if r == 1
+            navimap = generate_navi_map(maze, ""; iprob=-1.0)
+        else
+            navimap = generate_navi_map(maze, ""; itemcountprobs=[0.0 0.0 0.05 0.05 0.1 0.1 0.1 0.1 0.1 0.2 0.2], iprob=0.3)
+        end
+
+        nodes, path = generate_path(maze, available)
+        segments = segment_path(nodes)
+        prefx = "turn to the "
+        
+        for i=1:(length(segments)-1)
+            tp, path = segments[i]
+            if tp == "move"
+                continue
+            end
+
+            mname = join(rand(CHARS, 20))
+            navimap.name = mname
+            nbs = nodes_visible(navimap, path[1][1:2])#Array of arrays
+            tid = 0
+            tl = getlocation(navimap, path[end], 1)#target location
+            for j=1:length(nbs)
+                if nbs[j][1] == tl[1:2]
+                    tid = j
+                end
+            end
+            mcands = movecands(segments[i+1][2])
+            move_s = rand(mcands)
+            if r == 1
+                sits = shuffle(items)
+                n1 = path[1]
+
+                navimap.nodes[n1[1:2]] = Items[rand(sits[2:end])]#put an item on the current node
+                
+                for j=1:length(nbs)
+                    curr = path[1][1:2]
+                    trg = 0
+                    if j == tid
+                        trg = rand(1:length(nbs[j]))
+                        navimap.nodes[nbs[j][trg]] = Items[sits[1]]
+                    end
+                    for nb in nbs[j]
+                        if j == tid && nb == nbs[j][trg]
+                            continue
+                        end
+                        navimap.nodes[nb] = Items[rand(sits[2:end])]
+                    end
+                end
+                append!(path, segments[i+1][2][2:end])
+                ins = Instruction(name, split(string(prefx, sits[1], " and ", move_s)), path, mname, id)
+            else 
+                if r == 2#floor
+                    sflrs = shuffle(floors)
+                    for j=1:length(nbs)
+                        curr = path[1][1:2]
+                        for nb in nbs[j]
+                            w,f = navimap.edges[curr][nb]
+                            navimap.edges[curr][nb] = (w, Floors[sflrs[j]])
+                            navimap.edges[nb][curr] = (w, Floors[sflrs[j]])
+                            curr = nb
+                        end
+                    end
+                    append!(path, segments[i+1][2][2:end])
+                    ins = Instruction(name, split(string(prefx, sflrs[tid], " and ", move_s)), path, mname, id)
+                elseif r == 3#wall
+                    swlls = shuffle(walls)
+                    for j=1:length(nbs)
+                        curr = path[1][1:2]
+                        for nb in nbs[j]
+                            w,f = navimap.edges[curr][nb]
+                            navimap.edges[curr][nb] = j == tid ? (Walls[swlls[1]], f) : (Walls[swlls[rand(2:3)]], f)
+                            navimap.edges[nb][curr] = j == tid ? (Walls[swlls[1]], f) : (Walls[swlls[rand(2:3)]], f)
+                            curr = nb
+                        end
+                    end
+                    append!(path, segments[i+1][2][2:end])
+                    ins = Instruction(name, split(string(prefx, swlls[1], " and ", move_s)), path, mname, id)
+                end
+            end
+            break
+        end
+    end
+    return ins, navimap
+end
+
+function move_to_x_and_turn(name, id)
+    h,w = (8,8)
+    ins = nothing
+    navimap = nothing
+
+    while ins == nothing
+        maze, available = generate_maze(h, w; numdel=1)
+
+        r = rand(1:4)#1:item or end/wall/segment/intersection
+        if r == 1
+            navimap = generate_navi_map(maze, ""; iprob=-1.0)
+        else
+            navimap = generate_navi_map(maze, ""; itemcountprobs=[0.0 0.0 0.05 0.05 0.1 0.1 0.1 0.1 0.1 0.2 0.2], iprob=0.3)
+        end
+
+        nodes, path = generate_path(maze, available)
+        segments = segment_path(nodes)
+        prefx = "move to the "
+        
+        for i=1:length(segments)
+            tp, path = segments[i]
+            if tp == "turn"
+                continue
+            end
+
+            mname = join(rand(CHARS, 20))
+            navimap.name = mname
+            nbs = nodes_visible(navimap, path[1][1:2])#Array of arrays
+            tid = 0
+            tl = path[end]#target location
+            for j=1:length(nbs)
+                for nb in nbs[j]
+                    if nb == tl[1:2]
+                        tid = j
+                        break
+                    end
+                end
+            end
+            
+            endpoint = map(x->round(Int, x), path[end])
+            d = round(Int, endpoint[3] / 90 + 1)
+            steps = length(path)-1
+            
+            if (r > 1 && r < 4 && !facing_wall(maze, (endpoint[2], endpoint[1], d)) || (r == 4 && steps > 4))
+                continue
+            end
+
+            tcands = turncands(segments[i+1][2])
+            turn_s = rand(tcands)
+            if r == 1
+                sits = shuffle(items)
+                n1 = path[1]
+
+                navimap.nodes[n1[1:2]] = Items[rand(sits[2:end])]#put an item on the current node
+                
+                for j=1:length(nbs)
+                    curr = path[1][1:2]
+                    if j == tid
+                        navimap.nodes[tl[1:2]] = Items[sits[1]]
+                    end
+                    for nb in nbs[j]
+                        if j == tid && nb == tl[1:2]
+                            continue
+                        end
+                        navimap.nodes[nb] = Items[rand(sits[2:end])]
+                    end
+                end
+                
+                append!(path, segments[i+1][2][2:end])
+                ins = Instruction(name, split(string(prefx, sits[1], " and ", turn_s)), path, mname, id)
+            elseif r < 4
+                suffx = r == 2 ? "end" : "wall"
+                append!(path, segments[i+1][2][2:end])
+                ins = Instruction(name, split(string(prefx, suffx, " and ", turn_s)), path, mname, id)
+            else
+                ord = steps == 1 ? rand(["next", "first"]) : ordinals[steps]
+                suffx = " segment"
+                
+                append!(path, segments[i+1][2][2:end])
+                ins = Instruction(name, split(string(prefx, ord, suffx, " and ", turn_s)), path, mname, id)
+            end
+            break
+        end
+    end
+    return ins, navimap
+end
+
 """
 Available task functions:
 
-turn_to_x
-move_to_x
-to_x : generate data using turn_to_x and move_to_x
-turn_and_move_to_x
-lang_only
+t1: turn_to_x
+t2: move_to_x
+t3: combined_12 : generate data using turn_to_x and move_to_x
+t4: turn_and_move_to_x
+t5: lang_only
+t6: combined_1245 : generate data using turn_to_x, move_to_x, turn_and_move_to_x, lang_only
+t7: turn_to_x_and_move : the move part is lang only
+t8: move_to_x_and_turn : the turn part is lang only
 """
 function generatedata(taskf; numins=100)
     data = Any[]
