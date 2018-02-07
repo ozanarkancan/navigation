0fc37e0fc41c1493fe7ede792cbf373d4e64c7f0
ozanarkancan <can.ozanarkan@gmail.com>
2018-02-07 12:55:39 +0300

    shuffle instances

--- hyperopt.jl
+++ hyperopt.jl
@@ -14,12 +14,12 @@ function parse_commandline()
         ("--epoch"; help = "number of epochs"; default = 50; arg_type = Int)
         ("--patience"; help = "patience param"; default = 10; arg_type = Int)
         ("--limactions"; arg_type = Int; default = 20)
-        ("--window1"; help = "first dimension of the filters"; default = [1, 5, 1]; arg_type = Int; nargs = '+')
-        ("--window2"; help = "second dimension of the filters"; default = [5, 5, 12]; arg_type = Int; nargs = '+')
-        ("--filters"; help = "number of filters"; default = [40, 80, 50]; arg_type = Int; nargs = '+')
+        ("--window1"; help = "first dimension of the filters"; default = [1, 5]; arg_type = Int; nargs = '+')
+        ("--window2"; help = "second dimension of the filters"; default = [5, 12]; arg_type = Int; nargs = '+')
+        ("--filters"; help = "number of filters"; default = [40, 80]; arg_type = Int; nargs = '+')
         ("--model"; help = "model file"; default = "flex.jl")
-        ("--encdrops"; help = "dropout rates"; nargs = '+'; default = [0.0, 0.0]; arg_type = Float64)
-        ("--decdrops"; help = "dropout rates"; nargs = '+'; default = [0.0, 0.0]; arg_type = Float64)
+        ("--encdrops"; help = "dropout rates"; nargs = '+'; default = [0.6]; arg_type = Float64)
+        ("--decdrops"; help = "dropout rates"; nargs = '+'; default = [0.5, 0.8]; arg_type = Float64)
         ("--bs"; help = "batch size"; default = 1; arg_type = Int)
         ("--gclip"; help = "gradient clip"; default = 5.0; arg_type = Float64)
         ("--winit"; help = "scale the xavier"; default = 13.5; arg_type = Float64)
@@ -43,9 +43,10 @@ function parse_commandline()
         ("--beamsize"; help = "world attention"; arg_type = Int; default = 10)
         ("--beam"; help = "activate beam search"; action = :store_true)
         ("--globalloss"; help = "use global loss"; action = :store_true)
+        ("--singlerun"; help = "Single run with given params"; action = :store_true)
         ("--level"; help = "log level"; default="info")
         ("--taskf"; help = "task folder"; default="")
-        ("--limit"; help = "the error limit"; default=0.075; arg_type=Float64)
+        ("--limit"; help = "the error limit"; default=0.1; arg_type=Float64)
     end
     return parse_args(s)
 end		
@@ -115,8 +116,8 @@ function pretrain(instances, maps, vocab, emb, args)
         average_a += tst_acc * 100.0
         count += 100.0
 
-        avg_lss = numins < 20 ? average_l / count : avg_lss*0.99 + 0.01*lss
-        avg_acc = numins < 20 ? average_a / count : avg_acc*0.99 + 0.01*tst_acc
+        avg_lss = numins < 20 ? average_l / count : avg_lss*0.95 + 0.05*lss
+        avg_acc = numins < 20 ? average_a / count : avg_acc*0.95 + 0.05*tst_acc
 
         info("Batch: $numins , Loss: $lss , Acc: $(tst_acc)")
 
@@ -140,48 +141,45 @@ function pretrain(instances, maps, vocab, emb, args)
         end
     end
 
-    if !args["hopt"]
-        writetable(args["savecsv"], df)
-    end
     return (numins, w)
 end
 
 function hyperopt(instances, maps, vocab, emb, args)
     function xform_grid(x)
-        winit,hidden,embl,f1,f2,f3,watt = exp.(x) .* [10, 80.0, 80.0, 40, 50, 80, 50]
+        winit,hidden,embl,f1,f2,watt = exp.(x) .* [10.4597, 60.0, 120.0, 123, 155, 45]
         hidden = ceil(Int, hidden)
         embl = ceil(Int, embl)
         f1 = ceil(Int, f1)
         f2 = ceil(Int, f2)
-        f3 = ceil(Int, f3)
         watt = floor(Int, watt)
-        (winit,hidden,embl,f1,f2,f3,watt)
+        (winit,hidden,embl,f1,f2,watt)
     end
 
     function xform_other(x)
-        hidden,embl = exp.(x) .* [100.0, 100.0,]
+        hidden,embl,winit = exp.(x) .* [110.0, 100.0, 1.0]
         hidden = ceil(Int, hidden)
         embl = ceil(Int, embl)
-        (hidden,embl)
+        (hidden,embl,winit)
     end
 
     function f(x)
         srand(args["seed"])
         if args["percp"] && args["encoding"] == "grid" && args["worldatt"] != 0
-            winit, hidden, embl, f1, f2, f3, watt = xform_grid(x)
+            winit, hidden, embl, f1, f2, watt = xform_grid(x)
             args["winit"] = winit
             args["hidden"] = hidden
             args["embed"] = embl
-            args["filters"] = [f1, f2, f3]
+            args["filters"] = [f1, f2]
             args["worldatt"] = watt
             info("Config: ")
-            info("winit: $winit , hidden: $hidden , embed: $embl , filters: $([f1, f2, f3]) , worldatt: $watt ")
+            info("winit: $winit , hidden: $hidden , embed: $embl , filters: $([f1, f2]) , worldatt: $watt ")
         else
-            hidden, embl = xform_other(x)
+            hidden, embl, winit = xform_other(x)
             args["hidden"] = hidden
             args["embed"] = embl
+            args["winit"] = winit
             info("Config: ")
-            info("hidden: $hidden , embed: $embl ")
+            info("hidden: $hidden , embed: $embl , winit: $winit")
         end
 
         if args["hidden"] > 500 || args["embed"] > 500 || args["filters"][1] > 1500 || args["filters"][2] > 1500
@@ -193,10 +191,10 @@ function hyperopt(instances, maps, vocab, emb, args)
     end
 
     if args["percp"] && args["encoding"] == "grid" && args["worldatt"] != 0
-        f0, x0 = goldensection(f, 7; verbose=true)
+        f0, x0 = goldensection(f, 6; verbose=true)
         xbest = xform_grid(x0)
    else
-        f0, x0 = goldensection(f, 2; verbose=true)
+        f0, x0 = goldensection(f, 3; verbose=true)
         xbest = xform_other(x0)
     end
     return f0, xbest
@@ -217,6 +215,7 @@ function main()
 
     info("Reading instances")
     instances = readinsjson(args["taskf"] * "/instructions.json")
+    shuffle!(instances)
     info("Reading maps")
     maps = readmapsjson(args["taskf"] * "/maps.json")
     
@@ -224,9 +223,13 @@ function main()
     emb = args["wvecs"] ? load("data/embeddings.jld", "vectors") : nothing
     info("\nVocab: $(length(vocab))")
 
-    f0, x0 = hyperopt(instances, maps, vocab, emb, args)
-    info("Best loss: $f0")
-    info("Best args: $x0")
+    if args["singlerun"]
+        pretrain(instances, maps, vocab, emb, args)
+    else
+        f0, x0 = hyperopt(instances, maps, vocab, emb, args)
+        info("Best loss: $f0")
+        info("Best args: $x0")
+    end
 end
 
 main()
